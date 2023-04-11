05bb4f46e2f6a1afe1aa47a06e3bfff0577d423e
Parkbb <pyj9088@gmail.com>
2023-04-12 08:53:56 +0900

    C#-4/12
    C# 강좌: C# 교과서 마스터하기 11. 관계형 연산자와 논리 연산자 사용하기

---
+++ 관계형연산자/관계형연산자/관계형연산자.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 관계형연산자
+{
+    class 관계형연산자
+    {
+        static void Main()
+        {
+            //관계형 연산자: < 작음, <= 작거나 같음, >큼, >= 크거나 같음, == 같음, != 다름
+            //논리 연산자: &&논리곱, ||논리합, !논리부정
+
+            int firstNum = 10;
+            int secondNum = 5;
+            bool r = false;
+
+            Console.WriteLine(firstNum > secondNum); //true
+
+            r = (firstNum <= secondNum); //flase
+            Console.WriteLine(r);
+
+            r = (firstNum == secondNum); // C Family : ==, VB Family : =
+            Console.WriteLine(r);
+
+            r = (firstNum != secondNum);
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ 논리연산자/논리연산자/논리연산자.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class 논리연산자
+{
+    static void Main()
+    {
+        //논리연산자
+        // && 연산자 : 둘 다 참일때에만 참, 즉, 하나라도 거짓이면 거짓
+        // || 연산자 : 하나라도 참이면 참, 즉, 둘 다 거짓일때에만 거짓
+        // ! 연산자 : 참이면 거지승로, 거짓이면 참으로
+
+        int i = 3;
+        int j = 5;
+        bool r = false;
+
+        r = (i == 3) && (j != 3); // r = true && true => true
+        Console.WriteLine(r);
+
+        r = (i == 4) || (j != 3); // r = false || true => true
+        Console.WriteLine(r);
+
+        r = (i == 4) || (j == 3); // r = false || false => false
+        Console.WriteLine(r);
+
+        r = (i >= 5); // r => false
+        Console.WriteLine(!r);
+    }
+
+}
