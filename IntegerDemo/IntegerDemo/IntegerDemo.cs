12364b3cdf9b1d6e5136a818faa2670c18560eef
Parkbb <pyj9088@gmail.com>
2023-04-11 16:55:08 +0900

    C#-08
    C# 강의 - C# 교과서 마스터하기 08. 사용자한테 얻은 정보를 변수에 저장하기

--- IntegerDemo/IntegerDemo/IntegerDemo.cs
+++ IntegerDemo/IntegerDemo/IntegerDemo.cs
@@ -82,7 +82,12 @@ class MyClass
 
         //래퍼 형식: int, string과 같은 기본 형식을 클래스 또는 구조체로 감싼 .NET 형식
 
+        int number1 = 1234; // int 키워드: 기본 형식
+        int number2 = 1234; // System.Int32 구조체: .NET 형식
 
+        string str1 = "안녕"; // string 키워드: 기본 형식
+        String str2 = "안녕"; // System.String 클래스: .NET 형식
+      
 
 
 
---
+++ ReadLineNote/ReadLineNote/ReadLineNote.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class ReadLineDemo
+{
+    static void Main()
+    {
+        Console.ReadLine(); // <- 이 시점에서 대기하는 효과
+
+        Console.Write("이름:_______\b\b\b\b");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}님 안녕하세요.");
+
+        int x = Console.Read();
+
+        Console.WriteLine(x); // 아스키코드(ASCII)
+
+        Console.WriteLine(Convert.ToChar(x)); // 실제 문자
+
+        int number = 10;
+        
+        string str = Convert.ToString(number, 2);
+
+        string str2 = Convert.ToString(number, 2).PadLeft(8, '0');
+
+        string str3 = Convert.ToString(number, 2).PadLeft(8, '_');
+
+        Console.WriteLine(str);
+        Console.WriteLine(str2);
+        Console.WriteLine(str3);
+
+
+    }
+
+}
