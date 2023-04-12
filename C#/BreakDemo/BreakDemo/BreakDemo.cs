4eca4eae5f5cf80aa3b940f2a7372b42d80d07a6
Parkbb <pyj9088@gmail.com>
2023-04-12 13:14:24 +0900

    C3-4/12
    C# 강의, C# 강좌 - C# 교과서 마스터하기 16. while 문과 do 문, foreach 문으로 반복 처리하기
    C# 강의, C# 강좌 - C# 교과서 마스터하기 17. break, continue, goto로 반복문 제어하기

---
+++ C#/BreakDemo/BreakDemo/BreakDemo.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+class BreakDemo
+{
+    static void Main()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i == 5)
+            {
+                break; // i == 5 일때 for문 종료
+            }
+            Console.Write($"{(i+1)}번 반복\t");
+        }
+        Console.WriteLine();
+
+        int j = 0;
+        while (true)
+        {
+            if (j == 5)
+            {
+                break;
+            }
+            Console.Write($"{(j + 1)}번 반복\t");
+            j++;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ C#/GoToDemo/GoToDemo/GoToDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class GoToDemo
+{
+    static void Main()
+    {   
+        //goto문 : 지정한 레이블로 이동하는 구문
+        Console.WriteLine("시작");
+        Start:
+        Console.WriteLine("0,1,2 중 하나를 입력: _\b");
+        int chapter = Convert.ToInt32(Console.ReadLine());
+
+        if (chapter == 1)
+        {
+            goto chapter1;
+        }
+        else if (chapter == 2)
+        {
+            goto chapter2
+        }
+        else 
+        {
+            goto End;
+        }
+
+        chapter1:
+        Console.WriteLine("1장입니다.");
+     chapter2:
+        Console.WriteLine("2장입니다.");
+
+        goto Start;
+        End:
+        Console.WriteLine("종료");
+    }
+}
