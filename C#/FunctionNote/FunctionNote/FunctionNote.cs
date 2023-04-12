e04de54be4fb4cdf1dde3314d6ff81efaf82a99b
Parkbb <pyj9088@gmail.com>
2023-04-12 14:28:17 +0900

    C#-4/12
    C# 강의, C# 강좌 - C# 교과서 마스터하기 19. 함수 사용하기

---
+++ C#/FunctionNote/FunctionNote/FunctionNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class FunctionNote
+{   
+    // 선언
+    static int Sum(int first, int second)
+    {
+        return first + second;
+    }
+
+    // 아래처럼도 표현 가능
+    //static void Sum2() => Console.WriteLine("합계");
+
+    // 호출: DotNet.exe 3 5
+    static void Main(string[] args)
+    {
+        //호출
+        //FunctionNote.Sum();
+        int first = Convert.ToInt32(args[0]);
+        int second = Convert.ToInt32(args[1]);
+
+        Console.WriteLine(Sum(first, second));
+        
+    }
+}
