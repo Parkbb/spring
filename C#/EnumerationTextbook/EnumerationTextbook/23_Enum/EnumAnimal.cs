d28d9d27fda244ad4c2e6ca3574d51ea57e5d1e2
Parkbb <pyj9088@gmail.com>
2023-04-13 08:51:38 +0900

    C#-4/13
    C# 강의, C# 강좌 - C# 교과서 마스터하기 23. 열거형 형식 사용하기

---
+++ C#/EnumerationTextbook/EnumerationTextbook/23_Enum/EnumAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationTextbook._23_Enum
+{
+    class EnumAnimal
+    {
+        //열거형(Enumeration): 하나의 이름으로 서로 관련있는 정수 값을 갖는 상수 집합을 정의
+        enum Animal {Mouse, Cow, Tiger};
+
+        static void Main()
+        {
+            Console.WriteLine(Animal.Mouse);
+        }
+
+    }
+}
---
+++ C#/EnumerationTextbook/EnumerationTextbook/23_Enum/EnumerationTextbook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EnumerationTextbook._23_Enum
+{
+
+    class EnumerationTextbook
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("red");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine($" {nameof(ConsoleColor.Yellow)} & {nameof(ConsoleColor.Green)}");
+            Console.ResetColor();
+
+
+        }
+    }
+}
