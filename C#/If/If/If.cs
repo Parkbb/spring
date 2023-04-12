2a3820ae328280a8c0bf17380848e10e34832f1c
Parkbb <pyj9088@gmail.com>
2023-04-12 11:02:51 +0900

    C#-4/12
    C# 교과서 마스터하기 13. 제어문 소개 및 if else 문

---
+++ C#/If/If/If.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If
+{
+    class If
+    {
+        static void Main()
+        {
+            int score = 90;
+
+            if (score >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else 
+            {
+                Console.WriteLine("F");
+            }
+
+            
+        }
+    }
+}
