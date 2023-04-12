398840099949b099533bb2141a27cebc6488ba18
Parkbb <pyj9088@gmail.com>
2023-04-12 11:17:30 +0900

    C#-4/12
    C# 강의, C# 강좌 - C# 교과서 마스터하기 14. switch 문으로 다양한 조건 처리하기

---
+++ C#/SwitchDescription/SwitchDescription/SwitchDescription.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class SwitchDescription
+{
+    static void Main()
+    {
+        Console.WriteLine("정수를 입력하세요.");
+        int answer = Convert.ToInt32(Console.ReadLine());
+
+
+        switch (answer)
+        {
+            case 1:
+                Console.WriteLine("1을 선택했습니다.");
+                break;
+
+            case 2:
+                Console.WriteLine("2을 선택했습니다.");
+                break;
+
+            case 3:
+                Console.WriteLine("3을 선택했습니다.");
+                break;
+
+            case 4:
+                Console.WriteLine("4을 선택했습니다.");
+                break;
+
+            default:
+                Console.WriteLine("그냥 찍었네요");
+                break;
+        }
+    }
+}
