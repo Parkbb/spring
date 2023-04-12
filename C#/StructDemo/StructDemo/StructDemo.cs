b84da26781cf829100fee0d4b2f8dddb5872b86b
Parkbb <pyj9088@gmail.com>
2023-04-12 17:29:06 +0900

    C#-4/12
    C# 강의, C# 강좌 - C# 교과서 마스터하기 22. 구조체 사용하기

---
+++ C#/StructDemo/StructDemo/StructDemo.cs
@@ -0,0 +1,21 @@
+﻿//[?] 구조체: 하나 이상의 변수 또는 배열을 묶어서 관리
+using System;
+
+struct Point
+{
+    public int X; //public 공용
+    public int Y;
+    private int Z; //private 전용
+}
+
+class StructDemo
+{
+    static void Main()
+    {
+        Point point;
+        point.X = 100;
+        point.Y = 200;
+
+        Console.WriteLine($"X: {point.X}, Y: {point.Y}");
+    }
+}
