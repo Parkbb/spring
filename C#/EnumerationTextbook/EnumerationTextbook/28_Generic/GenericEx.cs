a6c37ca33ceca5f4c0c1b16f6395811cad11a6c1
Parkbb <pyj9088@gmail.com>
2023-04-13 12:59:47 +0900

    C#-4/13
    C# 강의, C# 강좌 - C# 교과서 마스터하기 28. 제네릭 사용하기

---
+++ C#/EnumerationTextbook/EnumerationTextbook/28_Generic/GenericEx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EnumerationTextbook._28_Generic
+{
+    class GenericEx
+    {
+        static void Main()
+        {
+            // 제네릭: Cup<T>을 Cup of T로 발음하여 형식 매개 변수인 T에 따른 Cup 클래스의 개체 생성
+            List<int> numbers = new List<int>();
+            numbers.Add(10);
+            numbers.Add(100);
+            numbers.Add(80);
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            List<string> colors = new List<string>();
+            colors.Add("red");
+            colors.Add("green");
+            colors.Insert(0, "blue");
+
+
+            List<int> numbers2 = new List<int>();
+
+            numbers2.AddRange(Enumerable.Range(1, 10));
+
+            foreach (var number in numbers2)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+        }
+    }
+}
