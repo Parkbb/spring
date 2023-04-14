8da33e7e50441386a00a8dae908340d15689f684
Parkbb <pyj9088@gmail.com>
2023-04-14 10:11:16 +0900

    C#-4/14
    C# 교과서 마스터하기 30. LINQ(Language INtegrated Query) 링크

---
+++ C#/EnumerationTextbook/EnumerationTextbook/30_LINQ/LINQ_EX.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationTextbook._30_LINQ
+{
+    class LINQ_EX
+    {
+        static void Main(string[] args)
+        {
+            // LINQ : C# 언어에 직접 쿼리(Query) 기능을 통합하는 방식을 기반으로 하는 기술 집합 이름
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine(numbers.Average());
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Sum());
+
+            Console.WriteLine(numbers.Select(x => { return x * 2; }).Sum());
+        }
+    }
+}
