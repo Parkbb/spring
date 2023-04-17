c815967cabe7ed64e71ac806d54dcd122eb4bea3
Parkbb <pyj9088@gmail.com>
2023-04-17 16:51:08 +0900

    C#-4/17
    Merge Algorithm

---
+++ C#/EnumerationTextbook/EnumerationTextbook/31_Algorithm/MergeAlgorithm.cs
@@ -0,0 +1,58 @@
+﻿//[?] 2개의 정수 배열 합치기: 단, 2개의 배열은 오름차순으로 정렬되어 있다고 가정
+// 병합 알고리즘(Merge Algorithm): 오름차순으로 정렬되어 있는 정수 배열을 하나로 병합
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationTextbook._31_Algorithm
+{
+    class MergeAlgorithm
+    {
+        static void Main(string[] args)
+        {
+            //[1] Input
+            int[] first = { 1, 3, 5 };
+            int[] second = { 2, 4 };
+            int M = first.Length; int N = second.Length; // M:N 관행
+            int[] merge = new int[M + N]; //병합된 배열 담을 그릇
+            int i = 0; 
+            int j = 0; 
+            int k = 0;
+
+
+
+            //[2] Process
+            while (i < M && j < N)
+            {
+                if (first[i] <= second[j]) // 더 작은 값을 merge 배열에 저장
+                {
+                    merge[k++] = first[i++];
+                }
+                else
+                {
+                    merge[k++] = second[j++];
+                }
+            }
+            
+            while (i < M) // 첫 번째 배열이 끝까지 도달할 때까지
+            {
+                merge[k++] = first[i++];
+            }
+
+            while (j < N) // 첫 번째 배열이 끝까지 도달할 때까지
+            {
+                merge[k++] = first[j++];
+            }
+
+            //[3] Output
+            foreach (var m in merge)
+            {
+                Console.Write($"{m}\t");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ C#/EnumerationTextbook/EnumerationTextbook/31_Algorithm/SearchAlgorithm.cs
@@ -0,0 +1,57 @@
+﻿//[?] 정렬되어 있는 데이터를 이진 검색(이분 탐색)을 사용하여 반씩 나눠서 검색
+// 검색 알고리즘(Search Algorithm): 주어진 데이터에서 특정 데이터 찾기
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationTextbook._31_Algorithm
+{
+    class SearchAlgorithm
+    {
+        static void Main(string[] args)
+        {
+            //[1] Input
+            int[] data = { 1, 3, 4, 6, 9 }; // 오름차순으로 정렬되었다고 가정
+            int N = data.Length; // 의사코드
+            int search = 6; // 검색할 데이터
+            bool flag = false; // 플래그 변수: 찾으면 true 찾지못하면 false
+            int index = -1;
+
+
+            //[2] Process: 이진 검색(Binary Search): Full Scan -> Index Scan
+            int low = 0; // min: 낮은 인덱스
+            int high = N - 1; // max: 높은 인덱스
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (data[mid] == search)
+                {
+                    flag = true;
+                    index = mid;
+                    break; // 찾으면 플래그, 인덱스 저장 후 종료
+                }
+                if (data[mid] > search)
+                {
+                    high = mid - 1; // 찾을 데이터가 작으면 왼쪽 영역으로 이동
+                }
+                else
+                {
+                    low = mid + 1; // 찾을 데이터가 크면 오른쪽 영역으로 이동
+                }
+            }
+
+            //[3] Output
+            if (flag)
+            {
+                Console.WriteLine($"{search}을 {index} 위치에서 찾았습니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{search}을 찾지 못했습니다.");
+            }
+           
+        }
+    }
+}
