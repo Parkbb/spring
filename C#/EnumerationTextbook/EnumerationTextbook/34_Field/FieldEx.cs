49dc9b240826570d394bda81e1dff729d69952a6
Parkbb <pyj9088@gmail.com>
2023-04-19 10:53:06 +0900

    C#-4/19
    C# 교과서 마스터하기 34. 필드(Field) - (C# 기초 강의, C# 기초 강좌)

---
+++ C#/EnumerationTextbook/EnumerationTextbook/34_Field/FieldEx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Person
+{
+    private string name = "박용주";
+    private const int m_age = 20; // 필드 == 멤버 변수, 전역 변수
+    private readonly string NickName;
+    private string[] websites = { "닷넷코리아", "비주얼아카데미" };
+}
+
+public class Say
+{
+    private string message = "반갑습니다.";
+    public void Hi()
+    {
+        Console.WriteLine(this.message);
+    }
+}
+
+public class FieldEx
+{
+    private static string message = "안녕하세요.";
+    public static void Hi() => Console.WriteLine(message);
+    public static void Main(string[] args)
+    {
+        int number = 1234; // 변수, 지역 변수
+        Console.WriteLine(number);
+        Console.WriteLine(message);
+        Hi();
+        Say say = new Say(); say.Hi();
+    }
+}
