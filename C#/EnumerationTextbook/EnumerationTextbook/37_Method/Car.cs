c85fed3563ff63895580624796e5594adc9a3933
Parkbb <pyj9088@gmail.com>
2023-04-19 14:21:50 +0900

    C#-4/19
    C# 교과서 마스터하기 37. 메서드와 매개 변수

---
+++ C#/EnumerationTextbook/EnumerationTextbook/37_Method/Car.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ch37
+{ 
+    public class Car
+    {
+        public void Map(string title) => Console.WriteLine($"{title}");
+    }
+}
+
---
+++ C#/EnumerationTextbook/EnumerationTextbook/37_Method/Progran.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ch37
+{
+    public class Car
+    {
+        public void Map(string title) => Console.WriteLine($"[2] {title}");
+    }
+
+    class MethodAndParameter
+    {
+        static void Main(string[] args)
+        {
+            string title = "복사"; Console.WriteLine($"[1] {title}");
+            var car = new Car(); car.Map(title);
+            Console.WriteLine($"[3] {title}");
+        }
+    }
+}
+
+
