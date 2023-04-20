d216477d0ebf976b235acafe222350a416b87b4c
Parkbb <pyj9088@gmail.com>
2023-04-20 16:41:53 +0900

    C#-4/20
    C# 교과서 마스터하기 48. 제네릭 클래스 만들기 요약 설명
    C# 교과서 마스터하기 49. 확장 메서드 만들기 요약 설명

---
+++ C#/EnumerationTextbook/EnumerationTextbook/49_Extension Method/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationTextbook._49_Extension_Method
+{
+    static class StringExtension
+    {
+        public static string Five(this String message) => message.Substring(0,5);
+        public static string Ten(this String message) => message.Substring(0,10);
+        public static string AddElipsis(this String message) => message + "...";
+    }
+
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            string message = "안녕하세요. 반갑습니다. 또 만나요";
+            Console.WriteLine(message.Five());
+        }
+    }
+}
