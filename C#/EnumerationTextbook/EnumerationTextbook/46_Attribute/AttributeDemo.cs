bdcc41da6c216f050a6e6e9619c70909dd36d7a9
Parkbb <pyj9088@gmail.com>
2023-04-20 15:12:29 +0900

    C#-4/20
    C# 교과서 마스터하기 46. 특성과 리플렉션 요약 설명

---
+++ C#/EnumerationTextbook/EnumerationTextbook/46_Attribute/AttributeDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EnumerationTextbook._46_Attribute
+{
+    public class LuxuryAttribute : Attribute 
+    {
+        public LuxuryAttribute() => Console.WriteLine("럭셔리!!!");
+    }
+
+    [Luxury]
+    class Car
+    {   [Obsolete("다음 버전에 제거될 예정이니 Auto()를 사용하세요.")]
+        public void Manual() => Console.WriteLine("수동");
+        public void Auto() => Console.WriteLine("자동");
+        [Conditional("DEBUG")]
+        public void Test() => Console.WriteLine("테스트");
+
+    }
+
+    class AttributeDemo
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            Attribute.GetCustomAttributes(typeof(Car));
+            typeof(Car).GetCustomAttributes(false);
+            car.Manual();
+            car.Auto();
+            car.Test();
+        }
+    }
+}
