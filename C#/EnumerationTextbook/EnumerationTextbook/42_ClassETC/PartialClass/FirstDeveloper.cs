b9c97951b6ac9b74c8b29ef62a591c1ecb447a25
Parkbb <pyj9088@gmail.com>
2023-04-20 11:50:23 +0900

    C#-4/20
    C# 교과서 마스터하기 42. 클래스(Class) 기타, 부분 클래스, 메서드 체이닝, 불변 형식

---
+++ C#/EnumerationTextbook/EnumerationTextbook/42_ClassETC/PartialClass/FirstDeveloper.cs
@@ -0,0 +1,11 @@
+﻿//[1] Hello 클래스의 첫 번째 파일
+using System;
+
+
+namespace PartialClass
+{
+    public partial class Hello
+    {
+        public void Hi() => Console.WriteLine("FirstDeveloper.cs");
+    }
+}
