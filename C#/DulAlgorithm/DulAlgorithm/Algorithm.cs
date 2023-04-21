cc2e7541b114d09cffd4808ea92a2fd4053e30cd
Parkbb <pyj9088@gmail.com>
2023-04-21 10:25:10 +0900

    C#-4/21
    C# 교과서 마스터하기 51. 튜플(Tuple) - C# 강의, C# 강좌, C# 동영상
    C# 교과서 마스터하기 52. 클래스 라이브러리와 닷넷 스탠다드 - C# 강의, C# 강좌, C# 동영상

---
+++ C#/DulAlgorithm/DulAlgorithm/Algorithm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DulAlgorithm
+{
+    public class Algorithm
+    {
+        /// <summary>
+        /// 선택 정렬 알고리즘
+        /// </summary>
+        /// <param name="numbers">정수형 배열</param>
+        /// <returns>오름차순 정렬된 정수형 배열</returns>
+        public static int[] SelectionSort(int[] numbers)
+        {
+            //[1] Input
+            //Parameter로 받아옴
+
+            //[2] Process
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+            //[3] Output
+            return numbers;
+        }
+    }
+}
