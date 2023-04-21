9f83157203ef1a630210d411941e72f005c0aa3c
Parkbb <pyj9088@gmail.com>
2023-04-21 11:19:03 +0900

    C#-4/21
    C# 교과서 마스터하기 53. 테스트 프로젝트와 단위 테스트 - C# 강의, C# 강좌, C# 동영상

---
+++ C#/DulAlgorithm/TestProject1/AlgorithmClassTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class AlgorithmClassTest
+    {
+        [TestMethod]
+        public void SelectionSort_ShouldReturnSortArray()
+        {
+            Assert.AreEqual(10, 200);
+        }
+
+        [TestMethod]
+        public void SelectionSortTest()
+        {
+            //[1] Arrange (정렬), Setup
+            int[] arr = { 33, 22, 11 };
+
+            //[2] Act (동작), Execute
+            int[] results = DulAlgorithm.Algorithm.SelectionSort(arr);
+
+            //[3] Assert, Verify
+            Assert.AreEqual(11, results[0]); //true
+            Assert.AreEqual(22, results[0]); //flase
+            
+
+        }
+    }
+
+}
