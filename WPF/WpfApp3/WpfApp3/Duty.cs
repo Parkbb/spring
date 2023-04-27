7ab9b506a6b949f43beb0fa03bcf654d793a8077
Parkbb <pyj9088@gmail.com>
2023-04-27 13:09:54 +0900

    WPF
    (WPF강좌)ListBox와 LINQ쿼리데이터바인딩, 새창띄우기, 이벤트 및 델리게이트를 통한 메인 윈도우리스트박스리프레쉬(WPF동영상/WPF강의/C#동영상/C#강의/C#교육)

---
+++ WPF/WpfApp3/WpfApp3/Duty.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfApp3
+{
+    public enum DutyType
+    { 
+        Inner,
+        Outside
+    }
+
+    public class Duty
+    {
+        private string _name;
+        private DutyType _dutyType;
+
+        public Duty() { }
+        public Duty(string name, DutyType dutyType)
+        {
+            _name = name;
+            _dutyType = dutyType;
+        }   
+
+        public string DutyName
+        { 
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public DutyType DutyType 
+        {
+            get { return _dutyType; }
+            set { _dutyType = value; }  
+        }
+    }
+
+    public class Duties : ObservableCollection<Duty>
+    {
+        public Duties()
+        { 
+            Add(new Duty("SALES", DutyType.Outside));
+            Add(new Duty("LOGISTICS", DutyType.Outside));
+            Add(new Duty("IT", DutyType.Inner));
+            Add(new Duty("MARKETING", DutyType.Inner));
+            Add(new Duty("PROPORTION", DutyType.Outside));
+            Add(new Duty("HR", DutyType.Inner));
+        }
+    }
+}
---
+++ WPF/WpfApp3/WpfApp3/SubWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// SubWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        //메인 윈도우의 하단 ListBox를 Refresh 하기 위한 델리게이트
+        //메인 윈도우에서 직무추가 버튼을 클릭할 때 이벤트를 할당해 준다.
+        public Delegate UpdateActor;
+
+        public SubWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
